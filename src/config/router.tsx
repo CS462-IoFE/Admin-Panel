1e5d9d05c297bc971ae342bd369fd66b033b674e
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-02 20:19:08 +0800

    Init router

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -1,4 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home";
+import Calendar from "../pages/event/Calendar";
+import EventListing from "../pages/event/EventListing";
+import EventDetails from "../pages/event/EventDetails";
+import AddEvent from "../pages/event/AddEvent";
+import EditEvent from "../pages/event/EditEvent";
+import Reports from "../pages/Reports";
+import RootLayout from "../pages/RootLayout";
 
-export default createBrowserRouter([{ path: "/", element: <Home /> }]);
+export default createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: "", element: <Calendar /> },
+            { path: "/event", element: <EventListing /> },
+            { path: "/event/:id", element: <EventDetails /> },
+            { path: "/event/add", element: <AddEvent /> },
+            { path: "/event/:id/edit", element: <EditEvent /> },
+            { path: "/reports", element: <Reports /> },
+        ],
+    },
+]);
