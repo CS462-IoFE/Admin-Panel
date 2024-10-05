9436ba10d6f275663968b571245e39d1e2f59afd
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-05 19:34:15 +0800

    Complete layout for Event Listing Page

--- src/pages/event/EventListing.tsx
+++ src/pages/event/EventListing.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import EventListingActions from "../../components/event/actions/EventListingActions";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { eventFilterSchema } from "../../zod-schema/eventFilterSchema";
+import EventListingTable from "../../components/event/table/EventListingTable";
 
 interface EventListingProps {}
 
 const EventListing: React.FC<EventListingProps> = ({}) => {
-    return <>EventListing Page</>;
+    const formState = useForm({ resolver: zodResolver(eventFilterSchema) });
+    const { watch } = formState;
+    const values = watch();
+
+    useEffect(() => {
+        console.log(values);
+    }, [watch, values]);
+
+    return (
+        <Container maxWidth="xl" sx={{ my: 4 }}>
+            <Typography variant="h3" mb={4}>
+                Events Listing
+            </Typography>
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Grid item>
+                    <FormProvider {...formState}>
+                        <EventListingActions />
+                    </FormProvider>
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" color="success">
+                        Add New Event
+                    </Button>
+                </Grid>
+            </Grid>
+            <EventListingTable />
+        </Container>
+    );
 };
 
 export default EventListing;
