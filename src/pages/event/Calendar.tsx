d903f0d6e9ae84d9cbe3a9b31c8338ffa9e9f6ea
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-05 18:44:04 +0800

    Integrate fullcalendar to Calendar page

--- src/pages/event/Calendar.tsx
+++ src/pages/event/Calendar.tsx
@@ -1,9 +1,22 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 interface CalendarProps {}
 
 const Calendar: React.FC<CalendarProps> = ({}) => {
-    return <>Calendar Page</>;
+    return (
+        <Container maxWidth="xl" sx={{ my: 4 }}>
+            <Typography variant="h3" mb={6}>
+                Events Calendar
+            </Typography>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+            />
+        </Container>
+    );
 };
 
 export default Calendar;
