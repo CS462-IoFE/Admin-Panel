f789ccca9e1e20634fed434bdd39b0117615adc9
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-06 19:26:36 +0800

    Create DatePicker field

---
+++ src/components/common/form/DatePicker.tsx
@@ -0,0 +1,39 @@
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker as MUIDatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Controller, useFormContext } from "react-hook-form";
+import { TextFieldVariants } from "@mui/material";
+
+interface DatePickerProps {
+    variant?: TextFieldVariants;
+    name: string;
+    label: string;
+}
+
+const DatePicker: React.FC<DatePickerProps> = ({ name, label, variant }) => {
+    const { control } = useFormContext();
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({ field, fieldState }) => (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <MUIDatePicker
+                        {...field}
+                        label={label}
+                        slotProps={{
+                            textField: {
+                                variant: variant,
+                                helperText: fieldState.error?.message,
+                                error: !!fieldState.error,
+                            },
+                        }}
+                    />
+                </LocalizationProvider>
+            )}
+        />
+    );
+};
+
+export default DatePicker;
