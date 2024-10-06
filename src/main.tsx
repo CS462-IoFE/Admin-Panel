1bf5fdc78ae58ee862d788066813c1a853c7bbe5
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-06 19:26:02 +0800

    Add APIProvider with Google Maps key to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,19 @@ import { SnackbarProvider } from "notistack";
 import { RouterProvider } from "react-router-dom";
 import router from "./config/router.tsx";
 import defaultTheme from "./config/theme.ts";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider theme={defaultTheme}>
-            <QueryClientProvider client={queryClient}>
-                <SnackbarProvider>
-                    <CssBaseline />
-                    <RouterProvider router={router} />
-                </SnackbarProvider>
-            </QueryClientProvider>
-        </ThemeProvider>
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API}>
+            <ThemeProvider theme={defaultTheme}>
+                <QueryClientProvider client={queryClient}>
+                    <SnackbarProvider>
+                        <CssBaseline />
+                        <RouterProvider router={router} />
+                    </SnackbarProvider>
+                </QueryClientProvider>
+            </ThemeProvider>
+        </APIProvider>
     </React.StrictMode>
 );
