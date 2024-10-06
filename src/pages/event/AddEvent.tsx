f8d2909977d8afadec808d97f5bfff6bfed83d1d
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-06 19:28:22 +0800

    Completion of Add Event for Basic Event Details Section

--- src/pages/event/AddEvent.tsx
+++ src/pages/event/AddEvent.tsx
@@ -1,9 +1,36 @@
+import { Button, Container, Divider, Grid, Typography } from "@mui/material";
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import BasicEventDetailsSection from "../../components/event/section/form/BasicEventDetailsSection";
 
 interface AddEventProps {}
 
 const AddEvent: React.FC<AddEventProps> = ({}) => {
-    return <>AddEvent Page</>;
+    const formState = useForm();
+    const { handleSubmit } = formState;
+
+    return (
+        <Container maxWidth="xl" sx={{ my: 4 }}>
+            <Typography variant="h3" mb={4}>
+                Add Event
+            </Typography>
+            <FormProvider {...formState}>
+                <Grid container direction="column">
+                    <BasicEventDetailsSection />
+                    <Divider />
+
+                    <Divider />
+                </Grid>
+            </FormProvider>
+            <Button
+                onClick={handleSubmit((data) => console.log(data))}
+                variant="contained"
+                color="success"
+            >
+                Submit Event
+            </Button>
+        </Container>
+    );
 };
 
 export default AddEvent;
