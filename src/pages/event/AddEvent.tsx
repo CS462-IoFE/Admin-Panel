65b1079ede612c66c1f8983d3c40c304ded805fb
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-06 20:25:42 +0800

    Complete Add Event page

--- src/pages/event/AddEvent.tsx
+++ src/pages/event/AddEvent.tsx
@@ -2,11 +2,15 @@ import { Button, Container, Divider, Grid, Typography } from "@mui/material";
 import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import BasicEventDetailsSection from "../../components/event/section/form/BasicEventDetailsSection";
+import StaffPresentSection from "../../components/event/section/form/StaffPresentSection";
+import PossibleMeetingLocationSection from "../../components/event/section/form/PossibleMeetingLocationsSection";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addEventSchema } from "../../zod-schema/addEventSchema";
 
 interface AddEventProps {}
 
 const AddEvent: React.FC<AddEventProps> = ({}) => {
-    const formState = useForm();
+    const formState = useForm({ resolver: zodResolver(addEventSchema) });
     const { handleSubmit } = formState;
 
     return (
@@ -17,18 +21,23 @@ const AddEvent: React.FC<AddEventProps> = ({}) => {
             <FormProvider {...formState}>
                 <Grid container direction="column">
                     <BasicEventDetailsSection />
-                    <Divider />
-
-                    <Divider />
+                    <Divider sx={{ mb: 2, mt: 4 }} />
+                    <PossibleMeetingLocationSection />
+                    <Divider sx={{ mb: 2, mt: 4 }} />
+                    <StaffPresentSection />
+                    <Divider sx={{ my: 2 }} />
+                    <Grid item>
+                        <Button
+                            sx={{ float: "right" }}
+                            onClick={handleSubmit((data) => console.log(data))}
+                            variant="contained"
+                            color="success"
+                        >
+                            Submit Event
+                        </Button>
+                    </Grid>
                 </Grid>
             </FormProvider>
-            <Button
-                onClick={handleSubmit((data) => console.log(data))}
-                variant="contained"
-                color="success"
-            >
-                Submit Event
-            </Button>
         </Container>
     );
 };
