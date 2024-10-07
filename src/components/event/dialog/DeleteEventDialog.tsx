5b9e4eb34b4e4941b1069c1be585cd4dcf1785c6
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-07 15:26:55 +0800

    Createa new Dialog for handling event deletions

---
+++ src/components/event/dialog/DeleteEventDialog.tsx
@@ -0,0 +1,52 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from "@mui/material";
+import React, { useState } from "react";
+
+interface DeleteEventDialogProps {
+    id: string;
+    name: string;
+}
+
+const DeleteEventDialog: React.FC<DeleteEventDialogProps> = ({ id, name }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <Button
+                variant="contained"
+                color="error"
+                onClick={() => setOpen(true)}
+            >
+                Delete
+            </Button>
+            <Dialog open={open} onClose={() => setOpen(false)}>
+                <DialogTitle>Confirm Delete</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete {name}?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button color="secondary" onClick={() => setOpen(false)}>
+                        Cancel
+                    </Button>
+                    <Button
+                        color="error"
+                        variant="contained"
+                        onClick={() => setOpen(false)}
+                    >
+                        Confirm Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+};
+
+export default DeleteEventDialog;
