b2c6110cdaab875383d69d8a6167260a33c0c678
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-07 15:26:43 +0800

    Add interceptor to axios instance to include Bearer token

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -1,12 +1,17 @@
 import axios from "axios";
 
 const configureInstance = (instanceUrl: string) => ({
-    baseURL: import.meta.env.DEV ? 
-        `${import.meta.env.VITE_DEVELOPMENT_SERVER}/${instanceUrl}` :
-        `${import.meta.env.VITE_PRODUCTION_SERVER}/${instanceUrl}`,
+    baseURL: import.meta.env.DEV
+        ? `${import.meta.env.VITE_DEVELOPMENT_SERVER}/${instanceUrl}`
+        : `${import.meta.env.VITE_PRODUCTION_SERVER}/${instanceUrl}`,
     setTimeout: 1000,
 });
 
-
 // Configure the instances by replacing the string for calling configureInstance
 export const logInstance = axios.create(configureInstance(""));
+
+// Run this line to inject the user token on all request made from the above instance
+logInstance.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${import.meta.env.VITE_USER_TOKEN}`;
+    return config;
+});
