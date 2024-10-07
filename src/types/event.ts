9f3c50f8e4dd95ad7f75b468d692e7fd28033b21
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-07 16:14:49 +0800

    Add types for EventDetails page

--- src/types/event.ts
+++ src/types/event.ts
@@ -6,3 +6,29 @@ export interface EventListingItem {
     start_time: string;
     end_time: string;
 }
+
+export interface EventDetails {
+    name_en: string;
+    name_cn: string;
+    date: string;
+    start_time: string;
+    end_time: string;
+    location: Location;
+    meeting_location: string;
+    accessibility: boolean;
+    attendee_list: AttendeeListItem[];
+    staff_list: string[];
+}
+
+export interface Location {
+    formatted_address: string;
+    lat: number;
+    lon: number;
+    name: string;
+}
+
+export interface AttendeeListItem {
+    name: string;
+    status: string;
+    reason?: string;
+}
