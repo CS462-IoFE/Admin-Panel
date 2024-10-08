1d8a75cce2a8e3edb12fb90a97c793fad25872d9
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-08 13:21:30 +0800

    Create User Listing Page

---
+++ src/pages/user/UserListing.tsx
@@ -0,0 +1,53 @@
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import SingleSelectField from "../../components/common/form/SingleSelectField";
+import UserListingTable from "../../components/user/table/UserListingTable";
+
+interface UserListingProps {}
+
+const UserListing: React.FC<UserListingProps> = ({}) => {
+    const formState = useForm();
+
+    return (
+        <FormProvider {...formState}>
+            <Container maxWidth="xl" sx={{ my: 4 }}>
+                <Typography variant="h3" mb={4}>
+                    User Listing
+                </Typography>
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <SingleSelectField
+                        name="sort"
+                        label="Sort By"
+                        options={[
+                            {
+                                label: "Newest First",
+                                value: "date-descending",
+                            },
+                            {
+                                label: "Oldest First",
+                                value: "date-ascending",
+                            },
+                            {
+                                label: "A-Z",
+                                value: "name-ascending",
+                            },
+                            {
+                                label: "Z-A",
+                                value: "name-descending",
+                            },
+                        ]}
+                    />
+                </Grid>
+                <UserListingTable />
+            </Container>
+        </FormProvider>
+    );
+};
+
+export default UserListing;
