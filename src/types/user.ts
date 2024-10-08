5b763fade75489094bb9a7f6047933d6d724f652
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-08 16:04:56 +0800

    Add additional user types for User Details page

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,5 +1,21 @@
+import { EventListingItem } from "./event";
+
 export interface UserListingItem {
     id: string;
     name: string;
     date: string;
+}
+
+export interface UserDetails {
+    id: string;
+    name: string;
+    date: string;
+    attended_events: EventListingItem[];
+    remarks: Remark[];
+}
+
+export interface Remark {
+    id: string;
+    date: string;
+    text: string;
 }
