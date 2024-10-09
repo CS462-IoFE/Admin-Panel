1aa16943e246b77cda41fc15cad212acb1aa12c0
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-09 18:30:04 +0800

    Integrate User Listing Table wif BE

--- src/components/user/table/UserListingTable.tsx
+++ src/components/user/table/UserListingTable.tsx
@@ -12,122 +12,87 @@ import dayjs from "dayjs";
 import React from "react";
 import { useFormContext } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { UserListingItem } from "../../../types/user";
 import DeleteUserDialog from "../dialog/DeleteUserDialog";
 import { UserFilterI } from "../../../zod-schema/userFilterSchema";
+import useUsers from "../../../custom-hooks/react-query/user/useUsers";
 
 interface UserListingTableProps {}
-
-const rows: UserListingItem[] = [
-    {
-        id: "0",
-        name: "User 0",
-        date: "09 Jan 2024",
-    },
-    {
-        id: "1",
-        name: "User 1",
-        date: "10 Jan 2024",
-    },
-    {
-        id: "2",
-        name: "User 2",
-        date: "08 Jan 2024",
-    },
-    {
-        id: "3",
-        name: "User 3",
-        date: "08 Jan 2024",
-    },
-    {
-        id: "4",
-        name: "User 4",
-        date: "08 Jan 2024",
-    },
-    {
-        id: "5",
-        name: "User 5",
-        date: "08 Jan 2024",
-    },
-    {
-        id: "6",
-        name: "User 6",
-        date: "08 Jan 2024",
-    },
-    {
-        id: "7",
-        name: "User 7",
-        date: "08 Jan 2024",
-    },
-];
 
 const UserListingTable: React.FC<UserListingTableProps> = ({}) => {
     const { watch } = useFormContext<UserFilterI>();
     const [sort] = watch(["sort"]);
     const navigate = useNavigate();
+    const { data: users } = useUsers();
 
     return (
         <TableContainer component={Box}>
             <Table sx={{ minWidth: 800 }}>
                 <TableHead>
                     <TableRow component="th">
-                        {["User Name", "Join Date", "Actions"].map((str) => (
-                            <TableCell sx={{ fontWeight: 600 }} key={str}>
-                                {str}
-                            </TableCell>
-                        ))}
+                        {["User Name", "Join Date", "Role", "Actions"].map(
+                            (str) => (
+                                <TableCell sx={{ fontWeight: 600 }} key={str}>
+                                    {str}
+                                </TableCell>
+                            )
+                        )}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows
-                        .map((row) => ({
-                            ...row,
-                            date: dayjs(row.date, "DD MM YYYY"),
-                        }))
-                        .sort((a, b) => {
-                            const [method, direction] = sort.split("-");
-                            if (method === "date") {
-                                return (
-                                    direction === "descending"
-                                        ? a.date.isBefore(b.date)
-                                        : a.date.isAfter(b.date)
-                                )
-                                    ? 1
-                                    : -1;
-                            }
-                            return direction === "ascending"
-                                ? a.name.localeCompare(b.name)
-                                : b.name.localeCompare(a.name);
-                        })
-                        .map(({ id, name, date }) => (
-                            <TableRow
-                                key={id}
-                                sx={{
-                                    "&:last-child td, &:last-child th": {
-                                        border: 0,
-                                    },
-                                }}
-                            >
-                                {[name, date.format("DD MMM YYYY")].map(
-                                    (it, index) => (
+                    {users &&
+                        users
+                            .map((row) => ({
+                                ...row,
+                                date: dayjs(row.created_at),
+                            }))
+                            .sort((a, b) => {
+                                const [method, direction] = sort.split("-");
+                                if (method === "date") {
+                                    return (
+                                        direction === "descending"
+                                            ? a.date.isBefore(b.date)
+                                            : a.date.isAfter(b.date)
+                                    )
+                                        ? 1
+                                        : -1;
+                                }
+                                return direction === "ascending"
+                                    ? a.name.localeCompare(b.name)
+                                    : b.name.localeCompare(a.name);
+                            })
+                            .map(({ id, name, date, role }) => (
+                                <TableRow
+                                    key={id}
+                                    sx={{
+                                        "&:last-child td, &:last-child th": {
+                                            border: 0,
+                                        },
+                                    }}
+                                >
+                                    {[
+                                        name,
+                                        date.format("D MMM YYYY"),
+                                        role,
+                                    ].map((it, index) => (
                                         <TableCell key={`${id}-${index}`}>
                                             {it}
                                         </TableCell>
-                                    )
-                                )}
-                                <TableCell>
-                                    <Button
-                                        sx={{ mr: 2 }}
-                                        variant="contained"
-                                        color="primary"
-                                        onClick={() => navigate(`/user/${id}`)}
-                                    >
-                                        View
-                                    </Button>
-                                    <DeleteUserDialog {...{ id, name }} />
-                                </TableCell>
-                            </TableRow>
-                        ))}
+                                    ))}
+                                    <TableCell>
+                                        <Button
+                                            sx={{ mr: 2 }}
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={() =>
+                                                navigate(`/user/${id}`)
+                                            }
+                                        >
+                                            View
+                                        </Button>
+                                        <DeleteUserDialog {...{ id, name }} />
+                                    </TableCell>
+                                </TableRow>
+                            ))}
                 </TableBody>
             </Table>
         </TableContainer>
