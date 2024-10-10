a3601006730a654a4059a0fbe324c76ba800d775
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-10 19:20:10 +0800

    Fix types for users based on backend return data

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,22 +1,31 @@
-import { EventListingItem } from "./event";
+import { Location } from "./event";
 
 export interface UserListingItem {
     id: string;
     name: string;
     created_at: string;
+    email: string;
     role: string;
+}
+
+export interface AttendedEventListingItem {
+    _id: string;
+    event_name: string;
+    event_date: string;
+    start_time: string;
+    event_location: Location;
+    end_time: string;
 }
 
 export interface UserDetails {
     id: string;
     name: string;
     date: string;
-    attended_events: EventListingItem[];
+    attended_events: AttendedEventListingItem[];
     remarks: Remark[];
 }
 
 export interface Remark {
-    id: string;
     date: string;
     text: string;
 }
