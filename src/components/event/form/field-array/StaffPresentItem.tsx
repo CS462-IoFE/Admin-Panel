3edd67380486ba9cae52e2cdc1021c4e26310a9a
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-10 19:22:07 +0800

    Integrate backend for retrieving staffs available to add to events

--- src/components/event/form/field-array/StaffPresentItem.tsx
+++ src/components/event/form/field-array/StaffPresentItem.tsx
@@ -2,21 +2,36 @@ import { Button, Grid } from "@mui/material";
 import React from "react";
 import SingleSelectField from "../../../common/form/SingleSelectField";
 import { UseFieldArrayRemove } from "react-hook-form";
+import useStaffUsers from "../../../../custom-hooks/react-query/user/useStaffUsers";
 
 interface StaffPresentItemProps {
     index: number;
     remove: UseFieldArrayRemove;
 }
 
-const StaffPresentItem: React.FC<StaffPresentItemProps> = ({ index, remove }) => {
+const StaffPresentItem: React.FC<StaffPresentItemProps> = ({
+    index,
+    remove,
+}) => {
+    const { data: staffs } = useStaffUsers();
+
     return (
         <Grid container direction="row" columnGap={2} alignItems="center">
             <SingleSelectField
                 name={`staff_present.${index}.name`}
                 label={`Staff ${index + 1}`}
-                options={[]}
+                options={
+                    staffs?.map(({ name }) => ({
+                        label: name,
+                        value: name,
+                    })) ?? []
+                }
             />
-            <Button onClick={() => remove(index)} variant="contained" color="error">
+            <Button
+                onClick={() => remove(index)}
+                variant="contained"
+                color="error"
+            >
                 Remove
             </Button>
         </Grid>
