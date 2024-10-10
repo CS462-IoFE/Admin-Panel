6d6d0cac5e1969dca45caa2a7228850bb4bc81f3
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-10 19:21:11 +0800

    Integrate backend for AddEvent to post a new event

--- src/pages/event/AddEvent.tsx
+++ src/pages/event/AddEvent.tsx
@@ -1,17 +1,28 @@
-import { Button, Container, Divider, Grid, Typography } from "@mui/material";
-import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import BasicEventDetailsSection from "../../components/event/section/form/BasicEventDetailsSection";
-import StaffPresentSection from "../../components/event/section/form/StaffPresentSection";
-import PossibleMeetingLocationSection from "../../components/event/section/form/PossibleMeetingLocationsSection";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { addEventSchema } from "../../zod-schema/addEventSchema";
+import { Button, Container, Divider, Grid, Typography } from "@mui/material";
+import { enqueueSnackbar } from "notistack";
+import React, { useEffect } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import BasicEventDetailsSection from "../../components/event/section/form/BasicEventDetailsSection";
+import PossibleMeetingLocationSection from "../../components/event/section/form/PossibleMeetingLocationsSection";
+import StaffPresentSection from "../../components/event/section/form/StaffPresentSection";
+import useAddEvent from "../../custom-hooks/react-query/event/useAddEvent";
+import { AddEventFormI, addEventSchema } from "../../zod-schema/addEventSchema";
 
 interface AddEventProps {}
 
 const AddEvent: React.FC<AddEventProps> = ({}) => {
-    const formState = useForm({ resolver: zodResolver(addEventSchema) });
+    const navigate = useNavigate();
+    const formState = useForm<AddEventFormI>({
+        resolver: zodResolver(addEventSchema),
+    });
     const { handleSubmit } = formState;
+    const { mutate, isSuccess } = useAddEvent();
+
+    useEffect(() => {
+        if (isSuccess) navigate("/event");
+    }, [isSuccess]);
 
     return (
         <Container maxWidth="xl" sx={{ my: 4 }}>
@@ -29,7 +40,14 @@ const AddEvent: React.FC<AddEventProps> = ({}) => {
                     <Grid item>
                         <Button
                             sx={{ float: "right" }}
-                            onClick={handleSubmit((data) => console.log(data))}
+                            onClick={handleSubmit(
+                                (data) => mutate(data),
+                                () =>
+                                    enqueueSnackbar(
+                                        "There are invalid inputs, please check your form again!",
+                                        { variant: "error" }
+                                    )
+                            )}
                             variant="contained"
                             color="success"
                         >
