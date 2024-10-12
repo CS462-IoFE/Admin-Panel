d0340366a4f687638286f4343ea7d8e23a716f4c
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-12 15:52:15 +0800

    Fix usePopulateEventDetailsForm being buggy for field arrays

--- src/custom-hooks/form/usePopulateEventDetailsForm.ts
+++ src/custom-hooks/form/usePopulateEventDetailsForm.ts
@@ -8,12 +8,10 @@ const usePopulateEventDetailForm = (
     formState: UseFormReturn<EditEventFormI>,
     event?: EventDetails
 ) => {
-    const { setValue, getValues, reset } = formState;
+    const { reset } = formState;
 
     useEffect(() => {
         if (event) {
-            reset()
-
             const {
                 event_name,
                 event_name_cn,
@@ -26,20 +24,17 @@ const usePopulateEventDetailForm = (
                 description,
             } = event;
 
-            setValue("accessibility", wheelchair_accessible);
-            setValue("name_en", event_name);
-            setValue("name_cn", event_name_cn);
-            setValue("location", { ...event_location });
-            setValue("date", dayjs(event_date));
-            setValue("start_time", dayjs(start_time, "HHmm"));
-            setValue("end_time", dayjs(end_time, "HHmm"));
-            setValue("description", description);
-            setValue(
-                "staff_present",
-                staff.map((staff) => ({ name: staff }))
-            );
-
-            console.log(getValues());
+            reset({
+                accessibility: wheelchair_accessible,
+                name_en: event_name,
+                name_cn: event_name_cn,
+                location: { ...event_location },
+                date: dayjs(event_date),
+                start_time: dayjs(start_time, "HHmm"),
+                end_time: dayjs(end_time, "HHmm"),
+                description: description,
+                staff_present: staff.map((staff) => ({ name: staff })),
+            });
         }
     }, [event]);
 };
