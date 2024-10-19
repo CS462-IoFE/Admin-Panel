326a1e4cf1e431dd1b8657d89a930dca9062fcca
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-19 22:01:34 +0800

    Complete Calendar page integration

--- src/pages/event/Calendar.tsx
+++ src/pages/event/Calendar.tsx
@@ -1,11 +1,21 @@
-import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import FullCalendar from "@fullcalendar/react";
 import { Container, Typography } from "@mui/material";
 import React from "react";
+import useCalendarEvent from "../../custom-hooks/calendar/useCalendarEvent";
+import useEvents from "../../custom-hooks/react-query/event/useEvents";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
 
 interface CalendarProps {}
 
 const Calendar: React.FC<CalendarProps> = ({}) => {
+    const navigate = useNavigate();
+
+    const { data: pastEvents } = useEvents(false);
+    const { data: upcomingEvents } = useEvents(true);
+    const events = useCalendarEvent(pastEvents ?? [], upcomingEvents ?? []);
+
     return (
         <Container maxWidth="xl" sx={{ my: 4 }}>
             <Typography variant="h3" mb={6}>
@@ -14,6 +24,14 @@ const Calendar: React.FC<CalendarProps> = ({}) => {
             <FullCalendar
                 plugins={[dayGridPlugin]}
                 initialView="dayGridMonth"
+                events={events}
+                eventClick={({ event }) =>
+                    navigate(
+                        `/event/${event.id}/${
+                            dayjs(event.start).isAfter(dayjs()) ? "edit" : ""
+                        }`
+                    )
+                }
             />
         </Container>
     );
