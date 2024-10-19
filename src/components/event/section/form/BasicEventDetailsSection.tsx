449ea244a0940df163dbf59fb867bfb66ec11d29
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-19 20:14:18 +0800

    Integrate translation API calls to Basic Event Details Section

--- src/components/event/section/form/BasicEventDetailsSection.tsx
+++ src/components/event/section/form/BasicEventDetailsSection.tsx
@@ -1,5 +1,9 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import { useFormContext } from "react-hook-form";
+import { getTranslation } from "../../../../axios/google";
+import { AddEventFormI } from "../../../../zod-schema/addEventSchema";
+import { EditEventFormI } from "../../../../zod-schema/editEventSchema";
 import CheckBox from "../../../common/form/Checkbox";
 import DatePicker from "../../../common/form/DatePicker";
 import MapField from "../../../common/form/MapField";
@@ -11,10 +15,23 @@ interface BasicEventDetailsSectionProps {}
 const BasicEventDetailsSection: React.FC<
     BasicEventDetailsSectionProps
 > = ({}) => {
+    const { getValues, setValue } = useFormContext<
+        AddEventFormI | EditEventFormI
+    >();
+
     return (
         <Grid container direction="column" mb={4} rowGap={2}>
             <Grid container direction="row" columnGap={2}>
-                <TextField name="name_en" label="Event Name (EN)" />
+                <TextField
+                    name="name_en"
+                    label="Event Name (EN)"
+                    onBlurCapture={async () =>
+                        setValue(
+                            "name_cn",
+                            await getTranslation(getValues("name_en"))
+                        )
+                    }
+                />
                 <TextField name="name_cn" label="Event Name (CN)" />
                 <DatePicker name="date" label="Event Date" />
             </Grid>
@@ -25,8 +42,23 @@ const BasicEventDetailsSection: React.FC<
             </Grid>
             <Grid item>
                 <TextField
+                    onBlurCapture={async () =>
+                        setValue(
+                            "description_cn",
+                            await getTranslation(getValues("description"))
+                        )
+                    }
                     name="description"
-                    label="Description"
+                    label="Description (EN)"
+                    multiline
+                    rows={4}
+                    sx={{ width: 650 }}
+                />
+            </Grid>
+            <Grid item>
+                <TextField
+                    name="description_cn"
+                    label="Description (CN)"
                     multiline
                     rows={4}
                     sx={{ width: 650 }}
